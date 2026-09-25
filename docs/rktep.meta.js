3cc083f58b1514702b26fc7457a5306c9c742eee
elRottenKotten <3954911+RottenKotten@users.noreply.github.com>
2026-09-25 23:11:38 +0400

    o.8.3.1

--- docs/rktep.meta.js
+++ docs/rktep.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitch Emote Presets
 // @namespace    kttn.local
-// @version      0.8.2
+// @version      0.8.3
 // @updateURL    https://rottenkotten.github.io/twitch-emote-presets/rktep.meta.js
 // @downloadURL  https://rottenkotten.github.io/twitch-emote-presets/rktep.user.js
 // ==/UserScript==
